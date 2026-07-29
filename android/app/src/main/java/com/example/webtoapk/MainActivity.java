b77e636ef850980584e2ce285db5d88651b94dbe
rlay2770-beep <rlay2770@gmail.com>
2026-07-29 12:54:44 +0630

    Add WebView to MainActivity for loading web content
    Implement a WebView in MainActivity to load a default URL.

---
+++ android/app/src/main/java/com/example/webtoapk/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.webtoapk;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        myWebView = new WebView(this);
+        setContentView(myWebView);
+
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
+        myWebView.setWebViewClient(new WebViewClient());
+        
+        // Default target URL
+        myWebView.loadUrl("https://example.com");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+    }
